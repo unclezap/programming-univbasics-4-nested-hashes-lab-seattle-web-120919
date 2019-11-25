a8c616a070b663136d7750d665ccae54f15e8f6b
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 13:33:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-return programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+return programmer_hash
 
 end
