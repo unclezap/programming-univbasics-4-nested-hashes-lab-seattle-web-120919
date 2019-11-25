04bcef5df979ab1a20025ccbd2d2028bcd6eff99
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 13:28:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -111,6 +111,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = alans_new_info
+     puts programmer_hash
      return programmer_hash
      
 end
@@ -131,6 +132,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
 
 end
